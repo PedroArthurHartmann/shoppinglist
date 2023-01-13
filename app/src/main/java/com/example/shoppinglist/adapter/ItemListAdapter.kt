348d8c162d7fcd38814d05ff44494c0924ea90ff
PedroArthurHartmann <pedroaphartmann@gmail.com>
2023-01-13 15:05:28 -0300

    recycle viewer

--- app/src/main/java/com/example/shoppinglist/adapter/ItemListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/adapter/ItemListAdapter.kt
@@ -4,8 +4,9 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.databinding.ItemListBinding
+import com.example.shoppinglist.ui.CommonItemsActivity
 
-class ItemListAdapter(private val ingr: List<String>) :
+class ItemListAdapter(private val list: List<String>, private val listener: OnItemSelected) :
     RecyclerView.Adapter<ItemListAdapter.ViewHolder>() {
 
     class ViewHolder(val binding: ItemListBinding) : RecyclerView.ViewHolder(binding.root)
@@ -15,12 +16,15 @@ class ItemListAdapter(private val ingr: List<String>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.tvItem.text = ingr[position]
+        holder.binding.tvItem.text = list[position]
         holder.binding.btItem.setOnClickListener {
-
-
+            listener.onSelect(list[position])
         }
     }
 
-    override fun getItemCount() = ingr.size
+    override fun getItemCount() = list.size
+}
+
+interface OnItemSelected {
+    fun onSelect(item: String)
 }
--- app/src/main/java/com/example/shoppinglist/ui/CommonItemsActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/CommonItemsActivity.kt
@@ -2,15 +2,14 @@ package com.example.shoppinglist.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.allViews
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shoppinglist.R
 import com.example.shoppinglist.adapter.ItemListAdapter
+import com.example.shoppinglist.adapter.OnItemSelected
 import com.example.shoppinglist.databinding.CommonItemsActivityBinding
 
-class CommonItemsActivity : AppCompatActivity() {
+class CommonItemsActivity : AppCompatActivity(), OnItemSelected {
 
     private lateinit var binding: CommonItemsActivityBinding
 
@@ -19,9 +18,14 @@ class CommonItemsActivity : AppCompatActivity() {
         binding = CommonItemsActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.rvList.adapter = ItemListAdapter(listOf<String>("Cheese", "Rice", "Apples"))
+        binding.rvList.adapter = ItemListAdapter(listOf("Cheese", "Rice", "Apples"), this)
         binding.rvList.layoutManager = LinearLayoutManager(this)
+    }
 
-        
+    override fun onSelect(item: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("LIST_ITEMS", item)
+        setResult(69, intent)
+        finish()
     }
 }
